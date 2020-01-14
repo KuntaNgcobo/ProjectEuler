f3b8ee815882dbad1dfe2b9bd34ac322c34dd657
Kunta-Kinte <NGCTHA030@myuct.ac.za>
2020-01-15 00:59:24 +0200

    Problem 2 completed with python and js
    Problem 2 completed with python and js -  hoped it would be a more elegant solution h but oh well

---
+++ Challenges/Problem_3/Largest_prime_factor.js
@@ -0,0 +1,38 @@
+// KuntaNgcobo
+// Largest_prime_factor
+// comment: Solution Not Liked by I
+
+const prime = n => {
+  if (n % 2 === 0 && n ==! 2) {
+    return false;
+  }
+
+  let factorCap = Math.ceil(Math.sqrt(n));
+  for (var i = 3; i < factorCap; i += 2) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const largestPrimeFactor = number => {
+  if (prime(number)) {
+    return number;
+  }
+
+  let factorCap = number % 2 === 0 ? number / 2 : Math.ceil(number / 3);
+
+  let i = 2;
+  while(factorCap >= i){
+    if(number % i===0){
+      number = number / i;
+      if(prime(number)){
+        return number;
+      }
+    }else{
+      i++;
+    }
+  }
+  return 0;
+}
---
+++ Challenges/Problem_3/Largest_prime_factor.py
@@ -0,0 +1,39 @@
+# KuntaNgcobo
+# Largest_prime_factor
+# comment: Solution Not Liked by I
+
+import math
+
+def prime(n):
+  if (n % 2 == 0 and n != 2):
+    return False
+  
+  factorCap = math.ceil(math.sqrt(n))
+  for i in range(3, factorCap, 2): 
+    if (n % i == 0):
+      return False
+  
+  return True
+
+
+def largestPrimeFactor(number):
+  if (prime(number)):
+    return number
+  
+
+  factorCap = number / 2 if(number % 2 == 0) else math.ceil(number / 3)
+
+  i = 2
+  while(factorCap >= i):
+
+    if(number % i==0):
+      number = number / i
+
+      if(prime(number)):
+        return number
+      
+    else:
+      i += 1 
+    
+  
+  return 0
