d00cc0cc24d10d505f048be80ba75dfabcc2b3a5
KuntaNgcobo <NGCTHA030@myuct.ac.za>
2020-01-16 21:09:55 +0200

    Problem 6 completed and improvement to Problem 4 for python code

---
+++ Challenges/Problem_6/Sum_square_difference.js
@@ -0,0 +1,7 @@
+// KuntaNgcobo
+// Sum square difference
+
+const sumSquareDifference = n => {
+  return Math.pow(n, 4)/4 + Math.pow(n, 3)/6
+  - Math.pow(n, 2)/4 - n/6;
+}
---
+++ Challenges/Problem_6/Sum_square_difference.py
@@ -0,0 +1,5 @@
+# KuntaNgcobo
+# Sum square difference
+
+def sumSquareDifference(n):
+  return (n**4)/4 + (n**3)/6 - (n**2)/4 - n/6;
